7621e65f81895498de8d3278c132e933bb408e83
Evan James <ej20002011@hotmail.co.uk>
2023-12-20 17:39:26 +0000

    Added graceful shutting down of the process

--- services/_global/svcframework/svcframework.go
+++ services/_global/svcframework/svcframework.go
@@ -3,7 +3,11 @@ package svcframework
 import (
 	"config"
 	"fmt"
+	"os"
+	"os/signal"
 	"svcframework/queues"
+	"sync"
+	"syscall"
 )
 
 type Service interface {
@@ -14,6 +18,8 @@ type Service interface {
 
 	Execute(payload string) error
 }
+
+const NUM_WORKER_ROUTINES int = 4
 
 var g_queueTodo queues.Queue
 var g_queueErrored queues.Queue
@@ -26,14 +32,23 @@ func Run(service Service) {
 	g_queueTodo = queues.NewRedisQueue(queuePrefix + "TODO")
 	g_queueErrored = queues.NewRedisQueue(queuePrefix + "ERRORED")
 
-	service.OnStartup()
-
-	workChan := make(chan string)
-	for i := 0; i < 4; i++ {
-		go worker(i, workChan)
+	err := g_service.OnStartup()
+	if err != nil {
+		fmt.Printf("Failed so start the service: %s\n", err.Error())
+		panic(err)
 	}
 
-	err := g_queueTodo.Push("2, 2")
+	workChan := make(chan string)
+	var wg sync.WaitGroup
+	for i := 0; i < NUM_WORKER_ROUTINES; i++ {
+		go worker(i, &wg, workChan)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go processSignals(signals, &wg)
+
+	err = g_queueTodo.Push("2, 2")
 	if err != nil {
 		panic(err)
 	}
@@ -55,19 +70,56 @@ func Run(service Service) {
 
 		fmt.Println("Finished putting in queue")
 	}
-
-	// TODO: Gracefully handle shutdown of service
-	//service.OnShutdown()
 }
 
-func worker(id int, channel chan string) {
+func worker(id int, wg *sync.WaitGroup, channel chan string) {
 	for {
 		val := <-channel
+		wg.Add(1)
+
 		fmt.Printf("Worker %d executing job [%s]\n", id, val)
 		err := g_service.Execute(val)
 		if err != nil {
 			g_queueErrored.Push(val)
 			fmt.Printf("Worker %d added job [%s] to error queue\n", id, val)
 		}
+
+		wg.Done()
 	}
+}
+
+func processSignals(signalChan chan os.Signal, wg *sync.WaitGroup) {
+	for {
+		signal := <-signalChan
+		exit := false
+
+		fmt.Println()
+		switch signal {
+		case syscall.SIGINT:
+			fmt.Println("SIGINT")
+			exit = true
+		case syscall.SIGTERM:
+			fmt.Println("SIGTERM")
+			exit = true
+
+		}
+
+		if exit {
+			fmt.Println("Cleaning up and exiting")
+			fmt.Println("Waiting for worker threads to finish...")
+			wg.Wait()
+			fmt.Println("Worker threads finished")
+			cleanUp()
+		}
+	}
+}
+
+func cleanUp() {
+	err := g_service.OnShutdown()
+	if err != nil {
+		fmt.Printf("Failed so stop the service: %s\n", err.Error())
+		panic(err)
+	}
+
+	os.Exit(0)
 }
